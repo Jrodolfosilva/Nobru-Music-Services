59c043075026983e105060427e429c10111ae319
Jrodolfosilva <rodolfo_s@outlook.com.br>
2023-04-06 19:40:52 -0300

    Iniciação do projeto
    Estruturação do conteúdo e seções da landing page

---
+++ js/scritp.js
@@ -0,0 +1,64 @@
+const servicos = {
+
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    },
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    },
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    },
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    },
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    },
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    },
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    },
+    assessoriaArtistica:{
+        image:"",
+        title:"Assessoria Artística",
+        buttonText:"SAIBA MAIS",
+        description:"A Assessoria Artística ajuda artistas a expandir suas carreiras musicais. Esse serviço engloba várias atividades, como a venda de shows, negociações publicitárias e comercialização de feat's. A venda de shows é uma das principais atividades da assessoria artística. A empresa cuida desde a negociação do cachê, contratos, até logística do show, transporte e hospedagem.",
+        link:"www.whatsapp.com"
+    }
+
+
+
+}
+
+
